0e1f6661a58b320c47c411d763dd38ab00811848
SamsonLin <153011490@qq.com>
2019-08-11 21:27:00 +0800

    test:add test named should_return_playerB_when_call_getBestPlayer_then_give_2C3C4C5C9C_3D4D5D6D9D

--- pokerhands/src/test/java/com/pokerhands/handler/RefereeTest.java
+++ pokerhands/src/test/java/com/pokerhands/handler/RefereeTest.java
@@ -238,4 +238,17 @@ public class RefereeTest {
         Assert.assertEquals("playerA",result);
     }
 
+    @Test
+    public void should_return_playerB_when_call_getBestPlayer_then_give_2C3C4C5C9C_3D4D5D6D9D(){
+        //given
+        Player playerA = new Player();
+        playerA.setCards(Arrays.asList(new Card(2,"C"),new Card(3,"C"),new Card(4,"C"),new Card(5,"C"),new Card(9,"C")));
+        Player playerB = new Player();
+        playerB.setCards(Arrays.asList(new Card(3,"D"),new Card(4,"D"),new Card(5,"D"),new Card(6,"D"),new Card(9,"D")));
+        //when
+        String result = referee.getBestPlayer(playerA,playerB);
+        //then
+        Assert.assertEquals("playerB",result);
+    }
+
 }
