a2cd61273ed5bf0edc0bd389e117d602a3082921
SamsonLin <153011490@qq.com>
2019-08-11 20:28:13 +0800

    test: add test named should_return_playEven_when_call_getBestPlayer_then_give_2C3H4C7H8C_2D3S4D7S8D

--- pokerhands/src/test/java/com/pokerhands/handler/RefereeTest.java
+++ pokerhands/src/test/java/com/pokerhands/handler/RefereeTest.java
@@ -82,4 +82,17 @@ public class RefereeTest {
         Assert.assertEquals("playerB",result);
     }
 
+    @Test
+    public void should_return_playEven_when_call_getBestPlayer_then_give_2C3H4C7H8C_2D3S4D7S8D(){
+        //given
+        Player playerA = new Player();
+        playerA.setCards(Arrays.asList(new Card(2,"C"),new Card(3,"H"),new Card(4,"C"),new Card(7,"H"),new Card(8,"C")));
+        Player playerB = new Player();
+        playerB.setCards(Arrays.asList(new Card(2,"D"),new Card(3,"S"),new Card(4,"D"),new Card(7,"S"),new Card(8,"D")));
+        //when
+        String result = referee.getBestPlayer(playerA,playerB);
+        //then
+        Assert.assertEquals("play even",result);
+    }
+
 }
